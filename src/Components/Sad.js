7d49237d890e4b240e32ae95802cb3a4a78adab9
Ibrahim Abulaila <abeabulaila@gmail.com>
2022-01-05 18:37:04 -0500

    newly updated

--- src/Components/Sad.js
+++ src/Components/Sad.js
@@ -4,7 +4,7 @@ import sadCat from './Gifs/sadcat.gif'
 function Sad() {
     return (
         <div>
-            <h1>I criiiii</h1>
+            <h1>I criiii</h1>
             <div>
                 <iframe width="600" height="600" src="https://www.youtube.com/embed/videoseries?list=PLI5YP4PZ67tLJyLabS1NrUypSoVhmAELu&autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 <Stars />
